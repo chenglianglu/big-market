44d9d8434ed1c486f1c1620fb88f5217d0777d76
HiGPP <hanchenfreer@gmail.com>
2024-08-23 16:46:53 +0800

    feat:开发运维 第2节：第一阶段完成抽奖部署

--- big-market-app/src/test/java/cn/edu/zjut/test/domain/StrategyArmoryDispatchTest.java
+++ big-market-app/src/test/java/cn/edu/zjut/test/domain/StrategyArmoryDispatchTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.Scanner;
 
 /**
  * @description: some desc
